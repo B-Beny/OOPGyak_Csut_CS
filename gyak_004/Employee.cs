54c2d12aee1c1ab6a24d6bf66dec4a5efedd53d8
Bettes Benjámin <78744128+B-Beny@users.noreply.github.com>
2021-03-15 20:53:56 +0100

    5. Heti házi feladat

---
+++ gyak_004/Employee.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gyak_004
+{
+    class Employee
+    {
+        //private string name;
+        //private long salary;
+
+        private string name;
+        private long salary;
+
+        public Employee(string name, long salary)
+        {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public Employee(string name) : this(name,250000) { }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public long Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        public string GetName()
+        { 
+            return name; 
+        }
+
+        public void SetName(string value)
+        { 
+            name = value; 
+        }
+
+        public long GetSalary()
+        {
+            return salary;
+        }
+
+        public void SetSalary(long value)
+        {
+            salary = value;
+        }
+
+        public void IncreaseSalary(long salary)
+        {
+            this.salary += salary;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + ", salary: " + Salary + " Ft";
+        }
+
+        public bool IsSalaryInRange(long lower, long upper)
+        {
+            if (salary >= lower && salary <= upper)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public long GetTax()
+        {
+            return (long)(salary * 0.16);
+        }
+
+        /// <summary>
+        /// Has higher salary?
+        /// </summary>
+        /// <param name="HasHigherSalary"></param>
+        /// <returns>boolean value</returns>
+
+        public bool HasHigherSalary(Employee otherEmployee)
+        {
+            if (salary > otherEmployee.salary)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ gyak_004/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace gyak_004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("valami");
+
+            //employee.name = "Kiss Jozsef";
+            //employee.salary = 200000;
+
+            employee.SetName("Kiss Jozsef");
+            employee.Name = "Kiss Ibolya";
+            employee.SetSalary(100000);
+
+            Console.WriteLine(employee);
+            employee.IncreaseSalary(10000);
+            Console.WriteLine(employee);
+
+            Console.WriteLine(employee.IsSalaryInRange(100000, 120000));
+            Console.WriteLine($"Tax: {employee.GetTax()}");
+
+            Employee otherEmployee = new Employee("Nagy Jozsef",90000);
+
+            Console.WriteLine(employee.HasHigherSalary(otherEmployee));
+            Console.WriteLine(otherEmployee.HasHigherSalary(employee));
+
+            Employee[] employeeArray = new Employee[5];
+
+            for (int i = 0; i < employeeArray.Length; i++)
+            {
+                Console.Write("Add meg az alkalmazott nevet: ");
+                string Name = Console.ReadLine();
+                Console.Write("Add meg a fizeteset: ");
+                long Salary = Convert.ToInt64(Console.ReadLine());
+                employeeArray[i] = new Employee(Name,Salary);
+            }
+
+            ListArray(employeeArray);
+            Console.WriteLine($"Legjobban fizetett: {employeeArray[BestPaid(employeeArray)].Name}");
+            Console.WriteLine($"Darabszam: {CountSalaries(employeeArray, 50000, 80000)}");
+            Console.WriteLine($"Atlag: {AverageSalary(employeeArray)}");
+            Console.WriteLine($"Ado: {TaxSumma(employeeArray)}");
+
+        }
+
+        public static void ListArray(Employee[] employeeArray)
+        {
+            foreach (var item in employeeArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static int BestPaid(Employee[] employeeArray)
+        {
+            int max=0;
+
+            for (int i = 1; i < employeeArray.Length; i++)
+            {
+                if (employeeArray[max].Salary < employeeArray[i].Salary)
+                {
+                    max = i;
+                }
+   
+            }
+
+            return max;
+        }
+
+        public static int CountSalaries(Employee[] employeeArray, long lower, long upper)
+        {
+            int counter=0;
+
+            foreach (var item in employeeArray)
+            {
+                if (item.IsSalaryInRange(lower,upper))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        public static double AverageSalary(Employee[] employees)
+        {
+            long szum=0;
+
+            foreach (var item in employees)
+            {
+                szum += item.Salary;
+            }
+
+            return (double)szum / employees.Length;
+        }
+
+        public static long TaxSumma(Employee[] employees)
+        {
+            long szum=0;
+
+            foreach (var item in employees)
+            {
+                szum += item.GetTax();
+            }
+
+            return szum;
+        }
+    }
+}
