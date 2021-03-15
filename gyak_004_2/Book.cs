040f2b8b44511a9954abccc8735d6656c1862613
Bettes Benjámin <78744128+B-Beny@users.noreply.github.com>
2021-03-15 21:37:37 +0100

    4. Het konyves

---
+++ gyak_004_2/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gyak_004_2
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public int Price { get; set; }
+
+        public void PercentPriceInc(long percent)
+        {
+            Price = (int)(Price + Price * percent);
+        }
+
+        /// <summary>
+        /// Links the properties of the book together in a string, then writes it on console.
+        /// </summary>
+        /// <returns>string</returns>
+
+        public string LinkStringOut()
+        {
+            return $"Cim: {Title}, Szerzo: {Author}, Kiadas Eve: {Year}, Ara: {Price}";
+        }
+
+    }
+}
---
+++ gyak_004_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace gyak_004_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book Book = new Book();
+
+            Book.Author = "Karcsi";
+            Book.Title = "Az utolso ut";
+            Book.Year = 2021;
+            Book.Price = 5099;
+
+            Console.WriteLine(Book.LinkStringOut());
+        }
+    }
+}
